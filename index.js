ee02dabb5ef84d4314eb4672e94dcba07f40959c
sepidehchitsaz <70854585+sepidehchitsaz@users.noreply.github.com>
2021-05-06 20:57:47 -0400

    ux_ui_autism

---
+++ index.js
@@ -0,0 +1,20 @@
+// var bigImg = document.getElementById('big');
+// var subImg = document.getElementById('sub').getElementsByTagName('img');
+
+// for (let i = 0; i < subImg.length; i++) {
+// subImg[i].addEventListener('click' , full_image);
+    
+// }
+
+// function full_image() {
+// var imgSrc = this.getAttribute('src');
+// bigImg.innerHTML = "<img src="+imgSrc+">";
+
+    
+// }
+
+$(document).ready(function() {
+    $(".image-gallary").hide(300);
+
+
+});
